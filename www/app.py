040a246df6c5046b19ddee46963c4995f75232b7
maybexss <maybe_xss@163.com>
2017-10-26 22:02:37 +0800

    Signed-off-by: maybexss <maybe_xss@163.com>

--- www/app.py
+++ www/app.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "maybexss"
+
+'''
+async web application.
+'''
+
 import logging; logging.basicConfig(level=logging.INFO)
 
 import asyncio, os, json, time
@@ -6,13 +15,12 @@ from datetime import datetime
 from aiohttp import web
 
 def index(request):
-    return web.Response(body='<h1>Awesome</h1>')
+    return web.Response(body=b'<h1>Awesome</h1>')
 
-@asyncio.coroutine
-def init(loop):
+async def init(loop):
     app = web.Application(loop=loop)
     app.router.add_route('Get', '/', index)
-    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
 
